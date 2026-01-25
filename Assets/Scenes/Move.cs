a17f0df6d0899a6ef3b71ae703fbf71c6bbd9002
yoshimogura <nakatayoshifumi3@gmail.com>
2026-01-25 19:01:06 +0900

    アニメーション再生できた

--- Assets/Scenes/Move.cs
+++ Assets/Scenes/Move.cs
@@ -12,40 +12,53 @@ public class Move : MonoBehaviour
     private Vector2 moveInput;
     private Rigidbody rb;
     private bool isGrounded = true;
+    private Animator animator;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>(); // ← ここで取得
+
     }
     void FixedUpdate()
     {
-        Vector3 move = transform.forward * moveInput.y * moveSpeed * Time.fixedDeltaTime;
+        Vector3 move = Quaternion.Euler(0, 0, 0) * transform.forward
+        * moveInput.y * moveSpeed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + move);
     }
 
     void Update()
     {
         // W/S：前後移動
-        Vector3 forwardMove = transform.forward * moveInput.y * moveSpeed * Time.deltaTime;
+        Vector3 forwardMove = Quaternion.Euler(0, 0, 0) * transform.forward * moveInput.y * moveSpeed * Time.deltaTime;
         //ジャンプ
         transform.Translate(forwardMove, Space.World);
 
         // A/D：左右回転
         float rotation = moveInput.x * rotateSpeed * Time.deltaTime;
         transform.Rotate(0, rotation, 0);
+
+        animator.SetFloat("Speed", Mathf.Abs(moveInput.y) * 2f);
+
+
+
+
     }
 
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
+
     public void OnJump(InputValue value)
     {
         if (isGrounded)
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
+
+
         }
     }
 
